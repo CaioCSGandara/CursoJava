5939b623a066fef12f301a0942aec81b7b2da63f
CaioCSGandara <caiocsgandara@gmail.com>
2025-02-01 14:25:55 -0300

    exercicios classe TemporalAdjusters

--- src/main/java/academy/devdojo/maratonajava/javacore/datas/test/TemporalAdjustersTest01.java
+++ src/main/java/academy/devdojo/maratonajava/javacore/datas/test/TemporalAdjustersTest01.java
@@ -5,6 +5,9 @@ import com.sun.security.jgss.GSSUtil;
 import java.sql.SQLOutput;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
 import java.time.temporal.TemporalAdjusters;
 
 public class TemporalAdjustersTest01 {
@@ -21,5 +24,38 @@ public class TemporalAdjustersTest01 {
         System.out.println(now.with(TemporalAdjusters.next(DayOfWeek.THURSDAY)));
         System.out.println(now.with(TemporalAdjusters.previous(DayOfWeek.THURSDAY)));
         System.out.println(now.with(TemporalAdjusters.lastDayOfMonth()));
+
+
+        //EXERCICIOS
+
+        //Ex. 1: Encontrar proxima segunda feira a partir e uma data
+        System.out.println("Ex. 1: Encontrar proxima segunda feira a partir e uma data");
+
+        LocalDate l1 = LocalDate.parse("2025-02-05");
+        System.out.println(l1.with(TemporalAdjusters.next(DayOfWeek.MONDAY)));
+
+        //Ex.2 Encontrar último dia do mes x no ano y
+        System.out.println("\nEx.2 Encontrar último dia do mes x no ano y");
+        LocalDate l2 = LocalDate.of(2025, 02, 01);
+        System.out.println(l2.with(TemporalAdjusters.lastDayOfMonth()));
+
+        //Ex. 3: Encontrar primeiro sabado de mes x no ano y
+        System.out.println("\nEx. 3: Encontrar primeiro sabado de mes x no ano y");
+        LocalDate l3 = LocalDate.now().withDayOfMonth(1);
+        System.out.println(l3.with(TemporalAdjusters.nextOrSame(DayOfWeek.SATURDAY)));
+
+        //Ex. 4: Encontrar a proxima sexta-feira 13:
+        System.out.println("\nEx. 4: Encontrar a proxima sexta-feira 13:");
+        LocalDate l4 = LocalDate.now().withDayOfMonth(13);
+        while(l4.getDayOfWeek()!=DayOfWeek.FRIDAY) {
+            l4 = l4.plusMonths(1);
+        }
+        System.out.println(l4);
+
+        //Ex. 5: Encontrar a última sexta-feira do mes
+        System.out.println("\nEx. 5: Encontrar a última sexta-feira do mes");
+        LocalDate l5 = LocalDate.now();
+        System.out.println(l5.with(TemporalAdjusters.lastInMonth(DayOfWeek.FRIDAY)));
+
     }
 }
